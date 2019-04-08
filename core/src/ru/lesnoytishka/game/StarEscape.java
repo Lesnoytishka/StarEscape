e8ff21e5d8e76b9c8dd21faa047d02f125858f94
Dmitry Ozhigin <Lesnoytishka@gmail.com>
2019-04-08 12:30:33 +0300

    Initial commit

---
+++ core/src/ru/lesnoytishka/game/StarEscape.java
@@ -0,0 +1,55 @@
+package ru.lesnoytishka.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class StarEscape extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureRegion[] region = new TextureRegion[16];
+	Animation backgroundAnimation;
+	TextureRegion background;
+
+	float stateTime;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("CosmoAnim.png");
+		for (int i = 0; i < 16; i++) {
+			region[i] = new TextureRegion(img, (i * 800), 0, 268, 500);
+		}
+		backgroundAnimation = new Animation(0.033f, region);
+		/*region = new TextureRegion(img, 0, 0, 268, 500);*/
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0.5f, 0.7f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		stateTime += Gdx.graphics.getDeltaTime();
+		background = (TextureRegion) backgroundAnimation.getKeyFrame(stateTime, true);
+
+		batch.begin();
+		batch.draw(background,
+				0,
+				0,
+				Gdx.app.getGraphics().getWidth(),
+				Gdx.app.getGraphics().getHeight()
+		);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/ru/lesnoytishka/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package ru.lesnoytishka.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.lesnoytishka.game.StarEscape;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 800;
+		config.width = 500;
+		new LwjglApplication(new StarEscape(), config);
+	}
+}
